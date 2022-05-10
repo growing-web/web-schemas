a04a5bd67503879fb03e3d5a5368d6148b334573
vben <anncwb@126.com>
2022-05-10 15:18:03 +0800

    feat: export mjs file

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import webBuilder from '../web-builder.json'
+import wenModule from '../web-module.json'
+import wenRoutemap from '../web-routemap.json'
+
+export { webBuilder, wenModule, wenRoutemap }
